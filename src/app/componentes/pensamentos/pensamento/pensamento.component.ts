a6852ee2dfc1894f49eb195e8d50cb44055c68c1
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2023-06-15 08:17:08 -0300

    Diretivas

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
   styleUrls: ['./pensamento.component.css']
 })
+
 export class PensamentoComponent {
 
+  @Input() pensamento = {
+    id: '1',
+    conteudo: 'I Love Angula',
+    autoria: 'Minha autoria',
+    modelo: 'modelo3',
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 200) {
+      return 'pensamento-g'
+    } else {
+      return 'pensamento-p'
+    }
+  }
 }
